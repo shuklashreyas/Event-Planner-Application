e85f65b438b5ad3406a3091ffc684715d74f411b
shreyman21 <shuklashreyas@icloud.com>
2024-04-24 11:53:04 -0400

    fixing scheduling frame

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * The main class for the project.
- */
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
--- src/view/IEventPanelView.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-/**
- * This interface represents the view for the event panel.
- */
-public interface IEventPanelView {
-}
